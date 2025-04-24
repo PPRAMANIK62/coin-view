fe7afab8b64424f2d406018783e0611955f2f6f9
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2025-04-24 10:30:40 +0530

    init: vite ts shadcn react-query

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Toaster } from "sonner";
+import Index from "./pages";
+import NotFound from "./pages/not-found";
+
+function App() {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
